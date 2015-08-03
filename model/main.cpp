2881def384988da18e44523e6bacffacb8710e30
Chanhee Jeong <firnenemerald@gmail.com>
2015-08-03 23:50:29 +0900

    Modified main.cpp

--- model/main.cpp
+++ model/main.cpp
@@ -22,37 +22,38 @@ int main(int argc, char** argv)
 	unsigned int indices[] = { 0, 1, 2 };
 
 	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]), indices, sizeof(indices) / sizeof(indices[0]));
-	Mesh mesh2("res/monkey3.obj");
+	//Mesh mesh2("res/monkey3.obj");
 	Shader shader("res/basicShader");
 	Texture texture("res/fur.jpg");
-	Camera camera(glm::vec3(0, 0, -150.0), 70.0f, (float)DISPLAY_WIDTH/(float)DISPLAY_HEIGHT, 0.01f, 1000.0f); 
+	Camera camera(glm::vec3(0, 0, -3.0), 100.0f, (float)DISPLAY_WIDTH/(float)DISPLAY_HEIGHT, 0.01f, 1000.0f); 
 	Transform transform;
 
-	float counter = 0.0f;
+	//float counter = 0.0f;
 
 	while (!display.IsClosed()){
 		
-		display.Clear(0.0f, 0.15f, 0.3f, 1.0f);
+		display.Clear(0.2f, 0.0f, 0.0f, 0.8f);
 		
-		float sinCounter = sinf(counter);
-		float cosCounter = cosf(5*counter);
+		//float sinCounter = sinf(counter);
+		//float cosCounter = cosf(5*counter);
 
-		transform.GetPos().x = 0.5*sinCounter;
-		transform.GetPos().z = 0.5*cosCounter;
+		//transform.GetPos().x = 0.5*sinCounter;
+		//transform.GetPos().z = 0.5*cosCounter;
 		//transform.GetRot().z = counter;
-		transform.GetRot().z = 1* counter;
-		transform.GetRot().y = 5* counter;
-		transform.GetRot().x = 3* counter;
+		//transform.GetRot().z = 1* counter;
+		//transform.GetRot().y = 5* counter;
+		//transform.GetRot().x = 3* counter;
 		//transform.SetScale(glm::vec3(cosCounter, cosCounter, cosCounter));
-		transform.GetScale() *= (1+0.001*cosf(counter));
+		//transform.GetScale() *= (1+0.001*cosf(counter));
 		
 		shader.Bind();
 		texture.Bind(0);
 		shader.Update(transform, camera);
-		mesh2.Draw();
+		//mesh2.Draw();
+		mesh.Draw();
 
 		display.Update();
-		counter += 0.001f;
+		//counter += 0.001f;
 	}
 
 	return 0;
