3fb89ce9575857b8bc486785d40f451c4ce81c25
Chanhee Jeong <firnenemerald@gmail.com>
2015-08-04 23:18:31 +0900

    How to solve this unsolvable linking error?

---
+++ branch/main.cpp
@@ -0,0 +1,11 @@
+#include <GL/glew.h>
+#define GLFW_DLL
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello world!" << std::endl;
+	return 0;
+}
