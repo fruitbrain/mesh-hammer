4fe085a17c767f303231db36b4fe8d5956596471
Stephen Kim <stephen422@gmail.com>
2015-08-22 01:20:37 +0900

    Fix fucked up indentation

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@
 int main()
 {
 	glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Plum", nullptr, nullptr);
 	if (window == nullptr) {
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -20,21 +20,21 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
-	
+
 	// Initialize GLEW
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK) {
 		std::cout << "Failed to initialize GLEW" << std::endl;
 		return -1;
 	}
-	
+
 	glViewport(0, 0, 800, 600);
-	
+
 	while(!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 		glfwSwapBuffers(window);
 	}
-	
+
 	glfwTerminate();
 	return 0;
 }
