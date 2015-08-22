4f260341efc4212a7e4392edd72662d6504d08d1
Stephen Kim <stephen422@gmail.com>
2015-08-22 17:09:29 +0900

    Implement draw our square tutorial

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+
 // GLEW
 #define GLEW_STATIC
 #include <GL/glew.h>
+
 // GLFW
 #include <GLFW/glfw3.h>
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+// Shaders
+const GLchar* vertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 position;\n"
+	"void main()\n"
+	"{\n"
+	"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+	"}\0";
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+	"out vec4 color;\n"
+	"void main()\n"
+	"{\n"
+	"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\n\0";
 
 int main()
 {
@@ -21,6 +39,8 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 
+	glfwSetKeyCallback(window, key_callback);
+
 	// Initialize GLEW
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK) {
@@ -30,11 +50,122 @@ int main()
 
 	glViewport(0, 0, 800, 600);
 
-	while(!glfwWindowShouldClose(window)) {
+	GLfloat vertices[] = {
+	       0.5f,  0.5f, 0.0f,		
+	       0.5f, -0.5f, 0.0f,
+	      -0.5f, -0.5f, 0.0f,
+	      -0.5f,  0.5f, 0.0f
+	};
+	GLuint indices[] = {
+		0, 1, 3,
+		1, 2, 3
+	};
+
+	/* Compile shaders */
+	GLuint vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	// Check for compile errors
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "Error: vertex shader compilation failed: " << infoLog << std::endl;
+	}
+
+	GLuint fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// Check for compile errors
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "Error: fragment shader compilation failed: " << infoLog << std::endl;
+	}
+
+	/* Link shaders */
+	GLuint shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// Check for linking errors
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "Error: shader linking failed: " << infoLog << std::endl;		
+	}
+
+	// Discard shaders as they got linked into the program
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	/* Generate vertex array object */
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+
+	/* Generate vertex buffer object */
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+
+	/* Generate element buffer object */
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
+	
+	/* Save the "macro" */
+	glBindVertexArray(VAO);
+	
+	// Bind VBO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Bind EBO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Link vertex attributes (Important!)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	
+	glBindVertexArray(0);
+
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		
+	/* Render loop */
+	while (!glfwWindowShouldClose(window)) {
+		/* Check and call events */
 		glfwPollEvents();
+
+		/* Render */
+		// Reset screen
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		// Use the program
+		glUseProgram(shaderProgram);
+
+		// Run the "macro"
+		glBindVertexArray(VAO);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+	        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		/* Swap the buffers */
 		glfwSwapBuffers(window);
 	}
 
 	glfwTerminate();
 	return 0;
 }
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
