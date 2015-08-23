e91fd6818fe2ac93e392ace12f42d6b328a6d137
Stephen Kim <stephen422@gmail.com>
2015-08-23 18:23:13 +0900

    Implement local to screen coordinate conversion
    TODO:
    * Implement proper set_uniform(glm::mat4) overload function
    * Refactor shader.vert
    * (optional) Optimize by pre-calculating conversion matrix

--- main.cpp
+++ main.cpp
@@ -8,9 +8,16 @@
 // GLFW
 #include <GLFW/glfw3.h>
 
+// GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "shader.h"
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+const GLuint WIDTH = 800, HEIGHT = 600;
 
 GLfloat vertices[] = {
 	0.5f,  0.5f, 0.0f,
@@ -32,7 +39,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Plum", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Plum", nullptr, nullptr);
 	if (window == nullptr) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
@@ -70,6 +77,8 @@ int main()
 	// Bind VBO
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//float data = 1.0f;
+	//glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(GLfloat), &data);
 
 	// Bind EBO
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
@@ -95,11 +104,30 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// Transform
+		glm::mat4 model;
+		model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		glm::mat4 view;
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		glm::mat4 projection;
+		projection = glm::perspective(glm::radians(45.0f), (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
+		glm::mat4 trans;
+		//trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+		trans = glm::rotate(trans, (GLfloat)glfwGetTime() * 1.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+
 		// Shaders
 		GLfloat timeValue = glfwGetTime();
 		GLfloat greenValue = (sin(timeValue) / 2) + 0.5;
 		shader.use();
 		shader.set_uniform("ourColor", 0.0f, greenValue, 0.0f, 1.0f);
+		GLuint model_loc = glGetUniformLocation(shader.program, "model");
+		glUniformMatrix4fv(model_loc, 1, GL_FALSE, glm::value_ptr(model));
+		GLuint view_loc = glGetUniformLocation(shader.program, "view");
+		glUniformMatrix4fv(view_loc, 1, GL_FALSE, glm::value_ptr(view));
+		GLuint projection_loc = glGetUniformLocation(shader.program, "projection");
+		glUniformMatrix4fv(projection_loc, 1, GL_FALSE, glm::value_ptr(projection));
+		GLuint transform_loc = glGetUniformLocation(shader.program, "transform");
+		glUniformMatrix4fv(transform_loc, 1, GL_FALSE, glm::value_ptr(trans));
 
 		// Run the "macro"
 		glBindVertexArray(VAO);
