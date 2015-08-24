a0b2c8d7fe22ea6ef1221bea32dff5e773e52ab5
Stephen Kim <stephen422@gmail.com>
2015-08-24 21:08:51 +0900

    Enable forward compatibility in GLFW
    This enables OS X platforms to create GLFW window without erring.

--- main.cpp
+++ main.cpp
@@ -84,6 +84,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Plum", nullptr, nullptr);
