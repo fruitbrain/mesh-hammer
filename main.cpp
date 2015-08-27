16e7ab4ecf0ed98ba9f685bdd4691f0069e4390a
Stephen Kim <stephen422@gmail.com>
2015-08-27 21:15:53 +0900

    Refactor out drawing code into separate functions

--- main.cpp
+++ main.cpp
@@ -16,6 +16,13 @@
 #include "shader.h"
 
 GLFWwindow* window;
+glm::vec3 pos_light(1.2f, 1.0f, 2.0f);
+
+Shader* shader;
+Shader* shader_lamp;
+
+void drawFace(GLuint vao, Shader* shader);
+void drawLamp(GLuint vao, Shader* shader);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
 const GLuint WIDTH = 800, HEIGHT = 600;
@@ -106,42 +113,32 @@ int main()
 {
 	initialize();
 
-	/* Misc variables */
-	//glm::vec3 pos_light(1.0f);
-	glm::vec3 pos_light(1.2f, 1.0f, 2.0f);
-
 	/* Shaders */
-	Shader shader("shader.vert", "shader.frag");
-	Shader shader_lamp("shader.vert", "lamp.frag");
+	shader = new Shader("shader.vert", "shader.frag");
+	shader_lamp = new Shader("shader.vert", "lamp.frag");
 
 	/* Generate Opengl objects */
 	GLuint VBO;
 	glGenBuffers(1, &VBO);
 
-	GLuint EBO;
-	glGenBuffers(1, &EBO);
-
 	/* VAO for the container */
-	GLuint VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
+	GLuint vao_container;
+	glGenVertexArrays(1, &vao_container);
+	glBindVertexArray(vao_container);
 
 	// Bind VBO
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// Partial update example:
 	//float data = 1.0f;
 	//glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(GLfloat), &data);
-
-	// Bind EBO
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// Link vertex attributes (Important!)
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	//glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
 	/* VAO for the light */
@@ -166,46 +163,68 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// Container
-		glm::mat4 model;
-		model = glm::rotate(model, (GLfloat)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-		//model = glm::rotate(model, glm::radians(40.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-		glm::mat4 view;
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-		glm::mat4 projection;
-		projection = glm::perspective(glm::radians(75.0f), (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
-		shader.use();
-		shader.set_uniform("color_object", 1.0f, 0.5f, 0.31f);
-		shader.set_uniform("color_light", 1.0f, 1.0f, 1.0f);
-		shader.set_uniform("pos_light", pos_light.x, pos_light.y, pos_light.z);
-		shader.set_uniform("model", model);
-		shader.set_uniform("view", view);
-		shader.set_uniform("projection", projection);
-
-		glBindVertexArray(VAO);
-		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-		glBindVertexArray(0);
-
-		// Lamp
-		model = glm::mat4(); // Reuse model
-		model = glm::translate(model, pos_light);
-		model = glm::scale(model, glm::vec3(0.2f));
-		shader_lamp.use();
-		shader_lamp.set_uniform("model", model);
-		shader_lamp.set_uniform("view", view);
-		shader_lamp.set_uniform("projection", projection);
-
-		glBindVertexArray(vao_light);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-		glBindVertexArray(0);
+		drawFace(vao_container, shader);
+		drawLamp(vao_light, shader_lamp);
 
 		/* Swap the buffers */
 		glfwSwapBuffers(window);
 	}
 
 	glfwTerminate();
+
+	delete shader;
+	delete shader_lamp;
+
 	return 0;
+}
+
+/**
+   Single draw call for the container.
+*/
+void drawFace(GLuint vao, Shader* shader)
+{
+	/* Matrices */
+	glm::mat4 model;
+	model = glm::rotate(model, (GLfloat)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+	glm::mat4 view;
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+	glm::mat4 projection;
+	projection = glm::perspective(glm::radians(75.0f), (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
+
+	shader->use();
+	shader->set_uniform("color_object", 1.0f, 0.5f, 0.31f);
+	shader->set_uniform("color_light", 1.0f, 1.0f, 1.0f);
+	shader->set_uniform("pos_light", pos_light.x, pos_light.y, pos_light.z);
+	shader->set_uniform("model", model);
+	shader->set_uniform("view", view);
+	shader->set_uniform("projection", projection);
+
+	glBindVertexArray(vao);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glBindVertexArray(0);
+}
+
+/**
+   Single draw call for the lamp.
+*/
+void drawLamp(GLuint vao, Shader* shader)
+{
+	glm::mat4 model;
+	model = glm::translate(model, pos_light);
+	model = glm::scale(model, glm::vec3(0.2f));
+	glm::mat4 view;
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+	glm::mat4 projection;
+	projection = glm::perspective(glm::radians(75.0f), (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
+
+	shader->use();
+	shader->set_uniform("model", model);
+	shader->set_uniform("view", view);
+	shader->set_uniform("projection", projection);
+
+	glBindVertexArray(vao);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glBindVertexArray(0);
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
