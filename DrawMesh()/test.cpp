40b31d790d432e72bede788b1e9d8745f7e24509
Chanhee Jeong <firnenemerald@gmail.com>
2015-08-28 01:48:52 +0900

    Add additional function to directly return VBO vertex information vectors

--- DrawMesh()/test.cpp
+++ DrawMesh()/test.cpp
@@ -10,41 +10,41 @@ double strToFloat(std::vector<char> charvec);
 int main()
 {
 	DrawMesh();
-
+	
 	return 0;
 }
 
 std::vector<double> DrawMesh()
 {
 	std::cout << "Hello OpenGL" << std::endl;
-
+	
 	// opening an input stream for the file
-
+	
 	std::ifstream ifs("example.plum", std::ios::in|std::ios::binary);
-
+	
 	if(!ifs.is_open())
 	{
 		std::cerr << "ERROR : The file did not open." << std::endl;
 	}
-
+	
 	std::cout << "The file opened successfully. Reading data from file to a vector..." << std::endl;
-
+	
 	// creating a vector container for all of the vertex data
-
+	
 	std::vector<std::vector<char> > vc;
-
+	
 	// start a loop to find all the vertex data & store it in a vector
-
+	
 	char memblock[] = {' '};
 	std::vector<char> tempvec;
-
+	
 	while(true)
 	{
 		ifs.read(memblock, 1);
 		if(memblock[0] == 'v')
-		{
+		{	
 			ifs.seekg(1, std::ios::cur);
-
+			
 			while(true)
 			{
 				ifs.read(memblock, 1);
@@ -67,7 +67,7 @@ std::vector<double> DrawMesh()
 				}
 			}
 		}
-
+		
 		else
 		{
 			while(memblock[0] != 0x0d)
@@ -77,19 +77,77 @@ std::vector<double> DrawMesh()
 			ifs.seekg(1, std::ios::cur);
 		}
 	}
-
-	ifs.close();
+	
 	std::cout << "Reading data finished!" << std::endl;
-
+	
 	// change std::vector<std::vector<char> > into a std::vector<double> - char to double transformation
-
+	
 	std::vector<double> vertexList;
-
+	
 	for(int i=0; i<vc.size(); i++)
 	{
 		vertexList.push_back(strToFloat(vc[i]));
 	}
+	
+	// creating a facial vertex container for all of the VBO data
+	
+	std::vector<std::vector<char> > fc;
+	
+	// start a loop to make the data that will go into the VBO
+	
+	std::vector<double> VBOList;;
+	tempvec.clear();
+	
+	std::cout << "test" <<std::endl;
+	
+	ifs.seekg(1);
+	ifs.read(memblock, 1);
+	std::cout << memblock[0] << std::endl;
+	
+	while(true)
+	{
+		ifs.read(memblock, 1);
+		std::cout << memblock[0] << std::endl;
+		if(memblock[0] == 'f')
+		{	
+			ifs.seekg(1, std::ios::cur);
+				std::cout << "test" <<std::endl;
 
+			while(true)
+			{
+				ifs.read(memblock, 1);
+				if(memblock[0] != 0x20 && memblock[0] != 0x0d)
+				{
+					std::cout << memblock[0];
+					tempvec.push_back(memblock[0]);
+				}
+				else if(memblock[0] == 0x20)
+				{
+					fc.push_back(tempvec);
+					tempvec.clear();
+					std::cout << " ";
+				}
+				else
+				{
+					ifs.seekg(1, std::ios::cur);
+					std::cout << std::endl;
+					break;
+				}
+			}
+		}
+		
+		else
+		{
+			while(memblock[0] != 0x0d)
+				ifs.read(memblock, 1);
+			if(ifs.eof())
+				break;
+			ifs.seekg(1, std::ios::cur);
+		}
+	}
+	
+	ifs.close();
+	
 	return vertexList;
 }
 
@@ -97,7 +155,7 @@ double strToFloat(std::vector<char> charvec)
 {
 	int size = charvec.size();
 	double result = 0.0;
-
+	
 	if(charvec[0] == '-')
 	{
 		for(int i=3; i<size; i++)
