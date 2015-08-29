2a95b367e3e64ab891622206696f3207de55718e
Stephen Kim <stephen422@gmail.com>
2015-08-29 22:52:53 +0900

    Backup cube drawing program in examples

---
+++ examples/cube.cpp
@@ -0,0 +1,243 @@
+#include <iostream>
+#include <cmath>
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+// GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "shader.h"
+
+GLFWwindow* window;
+glm::vec3 pos_light(1.2f, 1.0f, 2.0f);
+
+Shader* shader_container;
+Shader* shader_lamp;
+
+void draw_container(GLuint vao, Shader* shader);
+void draw_lamp(GLuint vao, Shader* shader);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+// Cube
+GLfloat vertices[] = {
+    -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+     0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+     0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+     0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+     0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+     0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+};
+
+GLuint indices[] = {
+	0, 1, 3,
+	1, 2, 3
+};
+
+/**
+   Initialize everything needed to show a window and an OpenGL context.
+*/
+void initialize()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	window = glfwCreateWindow(WIDTH, HEIGHT, "Plum", nullptr, nullptr);
+	if (window == nullptr) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return;
+	}
+	glfwMakeContextCurrent(window);
+
+	glfwSetKeyCallback(window, key_callback);
+
+	// Initialize GLEW
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	/* OpenGL options */
+	glEnable(GL_DEPTH_TEST);
+}
+
+/**
+   Main program flow
+*/
+int main()
+{
+	initialize();
+
+	/* Shaders */
+	shader_container = new Shader("shader.vert", "shader.frag");
+	shader_lamp = new Shader("shader.vert", "lamp.frag");
+
+	/* Generate Opengl objects */
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+
+	/* VAO for the container */
+	GLuint vao_container;
+	glGenVertexArrays(1, &vao_container);
+	glBindVertexArray(vao_container);
+
+	// Bind VBO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// Partial update example:
+	//float data = 1.0f;
+	//glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(GLfloat), &data);
+
+	// Link vertex attributes (Important!)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+	//glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	/* VAO for the light */
+	GLuint vao_light;
+	glGenVertexArrays(1, &vao_light);
+	glBindVertexArray(vao_light);
+
+	// No need to feed data this time
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// Only position data
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0);
+
+	/* Render loop */
+	while (!glfwWindowShouldClose(window)) {
+		/* Check and call events */
+		glfwPollEvents();
+
+		/* Render */
+		// Reset screen
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		draw_container(vao_container, shader_container);
+		draw_lamp(vao_light, shader_lamp);
+
+		/* Swap the buffers */
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+
+	delete shader_container;
+	delete shader_lamp;
+
+	return 0;
+}
+
+/**
+   Single draw call for the container.
+*/
+void draw_container(GLuint vao, Shader* shader)
+{
+	/* Matrices */
+	glm::mat4 model;
+	model = glm::rotate(model, (GLfloat)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+	glm::mat4 view;
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+	glm::mat4 projection;
+	projection = glm::perspective(glm::radians(75.0f), (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
+
+	shader->use();
+	shader->set_uniform("color_object", 1.0f, 0.5f, 0.31f);
+	shader->set_uniform("color_light", 1.0f, 1.0f, 1.0f);
+	shader->set_uniform("pos_light", pos_light.x, pos_light.y, pos_light.z);
+	shader->set_uniform("model", model);
+	shader->set_uniform("view", view);
+	shader->set_uniform("projection", projection);
+
+	glBindVertexArray(vao);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glBindVertexArray(0);
+}
+
+/**
+   Single draw call for the lamp.
+*/
+void draw_lamp(GLuint vao, Shader* shader)
+{
+	glm::mat4 model;
+	model = glm::translate(model, pos_light);
+	model = glm::scale(model, glm::vec3(0.2f));
+	glm::mat4 view;
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+	glm::mat4 projection;
+	projection = glm::perspective(glm::radians(75.0f), (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
+
+	shader->use();
+	shader->set_uniform("model", model);
+	shader->set_uniform("view", view);
+	shader->set_uniform("projection", projection);
+
+	glBindVertexArray(vao);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glBindVertexArray(0);
+}
+
+/**
+   Key callback function for GLFW.
+*/
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
