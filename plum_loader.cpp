93f76ae1161dafa56e73e19c44df21387be4dd14
Stephen Kim <stephen422@gmail.com>
2015-09-01 14:52:25 +0900

    Make vertex and face arrays nested
    Make mesh.vertex/face_array convey a more direct representation of the
    underlying structure of the .plum data file.
    
    By packing related coordinates / indices into different arrays, it
    prevents any faulty access to unrelated vertex coordinates in
    compile-time. Also, this representation allows storing faces that have varying numbers
    of vertices.
    
    But this also has disadvantage of difficulty in conversion between
    Scheme / C object, harder memory management and generally 'messier'
    code.

--- plum_loader.cpp
+++ plum_loader.cpp
@@ -20,20 +20,20 @@ extern "C" Mesh plum_loader(const char* filename)
 		std::cerr << "ERROR : The file did not open." << std::endl;
 
 	// vertex container for all of the vertex data
-	std::vector<std::vector<char> > vc;
+	std::vector<std::vector<float> > vc;
 	// facial vertex container for all of the VBO data
-	std::vector<std::vector<char> > fc;
+	std::vector<std::vector<int> > fc;
 
 	// start a loop to find all the vertex data & store it in vc
 	char memblock[1];
 
+	std::cout << "File content:" << std::endl;
 	while(true) {
-		// only used inside while loop, so declare here
-		std::vector<char> tempvec;
-
 		ifs.read(memblock, 1);
 
 		if(memblock[0] == 'v') {		// Vertex line
+			std::vector<float> vertex;
+			std::vector<char> tempvec;
 			ifs.seekg(1, std::ios::cur);
 
 			while (true) {
@@ -41,21 +41,28 @@ extern "C" Mesh plum_loader(const char* filename)
 
 				// 0x20: space, 0x0d: newline
 				if (memblock[0] != 0x20 && memblock[0] != 0x0d) {
-					std::cout << memblock[0];
+					/* Reading 1 coord */
 					tempvec.push_back(memblock[0]);
+					std::cout << memblock[0];
 				} else if (memblock[0] == 0x20) {
-					vc.push_back(tempvec);
+					/* Finished 1 coord */
+					vertex.push_back(char_vector_to_float(tempvec));
 					tempvec.clear();
 					std::cout << " ";
 				} else {
-					vc.push_back(tempvec);
+					/* Finished 1 line */
+					vertex.push_back(char_vector_to_float(tempvec));
+					vc.push_back(vertex);
 					tempvec.clear();
-					ifs.seekg(1, std::ios::cur);
 					std::cout << std::endl;
+
+					ifs.seekg(1, std::ios::cur);
 					break;
 				}
 			}
 		} else if (memblock[0] == 'f') {	// Face line
+			std::vector<int> face;
+			std::vector<char> tempvec;
 			ifs.seekg(1, std::ios::cur);
 
 			while (true) {
@@ -63,17 +70,22 @@ extern "C" Mesh plum_loader(const char* filename)
 
 				// 0x20: space, 0x0d: newline
 				if(memblock[0] != 0x20 && memblock[0] != 0x0d) {
-					std::cout << memblock[0];
+					/* Reading 1 index */
 					tempvec.push_back(memblock[0]);
+					std::cout << memblock[0];
 				} else if(memblock[0] == 0x20) {
-					fc.push_back(tempvec);
+					/* Finished 1 index */
+					face.push_back(char_vector_to_int(tempvec));
 					tempvec.clear();
 					std::cout << " ";
 				} else {
-					fc.push_back(tempvec);
+					/* Finished 1 line */
+					face.push_back(char_vector_to_int(tempvec));
+					fc.push_back(face);
 					tempvec.clear();
-					ifs.seekg(1, std::ios::cur);
 					std::cout << std::endl;
+
+					ifs.seekg(1, std::ios::cur);
 					break;
 				}
 			}
@@ -85,33 +97,51 @@ extern "C" Mesh plum_loader(const char* filename)
 			ifs.seekg(1, std::ios::cur);
 		}
 	}
+	std::cout << std::endl;
 	ifs.close();
 
 	// Convert vc and fc into float and int array
-	float* vertex_array = new float[vc.size()];
-	for (std::size_t i=0; i<vc.size(); i++)
-		vertex_array[i] = char_vector_to_float(vc[i]);
+	float** vertex_array = new float*[fc.size()];
+	for (std::size_t i=0; i<vc.size(); i++) {
+		vertex_array[i] = new float[3];
+		vertex_array[i][0] = vc[i][0];
+		vertex_array[i][1] = vc[i][1];
+		vertex_array[i][2] = vc[i][2];
+	}
 
-	int* face_array = new int[fc.size()];
-	for(std::size_t i=0; i<fc.size(); i++)
-		face_array[i] = char_vector_to_int(fc[i]);
+	// Caution: 0th element is the *size of that array*
+	int** face_array = new int*[fc.size()];
+	for (std::size_t i=0; i<fc.size(); i++) {
+		face_array[i] = new int[1+fc[i].size()];
+		face_array[i][0] = fc[i].size();
+		for (std::size_t j=0; j<fc[i].size(); j++)
+			face_array[i][j+1] = fc[i][j];
+	}
 
 	// Print arrays for debugging purpose
 	std::cout << "vertex_array:" << std::endl;
-	for (std::size_t i=0; i<vc.size(); i++)
-		std::cout << vertex_array[i] << " ";
+	for (std::size_t i=0; i<vc.size(); i++) {
+		std::cout << vertex_array[i][0] << " ";
+		std::cout << vertex_array[i][1] << " ";
+		std::cout << vertex_array[i][2] << std::endl;
+	}
 	std::cout << std::endl;
+
 	std::cout << "face_array:" << std::endl;
-	for (std::size_t i=0; i<fc.size(); i++)
-		std::cout << face_array[i] << " ";
+	for (std::size_t i=0; i<fc.size(); i++) {
+		int size = face_array[i][0];
+		for (int j=0; j<size; j++)
+			std::cout << face_array[i][j+1] << " ";
+		std::cout << std::endl;
+	}
 	std::cout << std::endl;
 
 	std::cout << "Mesh data loading completed" << std::endl;
 
 	// Pack the results into a struct.
 	Mesh mesh;
-	mesh.vertex_count = vc.size();	// XXX
-	mesh.face_count = fc.size();	// XXX
+	mesh.vertex_count = vc.size();
+	mesh.face_count = fc.size();
 	mesh.vertex_array = vertex_array;
 	mesh.face_array = face_array;
 
@@ -127,11 +157,12 @@ extern "C" GLfloat* plum_loader_vbo(const char* filename)
 	const int vbo_size = mesh.face_count * 3;
 	GLfloat* vbo_list = new GLfloat[vbo_size];
 
-	for (int i=0; i<mesh.face_count; i++) {
-		vbo_list[i*3]   = mesh.vertex_array[mesh.face_array[i]*3];
-		vbo_list[i*3+1] = mesh.vertex_array[mesh.face_array[i]*3+1];
-		vbo_list[i*3+2] = mesh.vertex_array[mesh.face_array[i]*3+2];
-	}
+	// FIXME
+	/*for (int i=0; i<mesh.face_count; i++) {
+		vbo_list[i*3] = mesh.vertex_array[mesh.face_array[i][0]] [0];
+		vbo_list[i*3+1] = mesh.vertex_array[mesh.face_array[i][1]] [1];
+		vbo_list[i*3+2] = mesh.vertex_array[mesh.face_array[i][2]] [2];
+		}*/
 
 	// Delete arrays of struct mesh
 	delete_mesh(mesh);
@@ -151,7 +182,12 @@ extern "C" GLfloat* plum_loader_vbo(const char* filename)
 */
 extern "C" void delete_mesh(Mesh mesh)
 {
+	std::cout << "Deleting Mesh..." << std::endl;
+	for (std::size_t i=0; i<mesh.vertex_count; i++)
+		delete [] mesh.vertex_array[i];
 	delete [] mesh.vertex_array;
+	for (std::size_t i=0; i<mesh.face_count; i++)
+		delete [] mesh.face_array[i];
 	delete [] mesh.face_array;
 }
 
--- test/plum_loader_test.c
+++ test/plum_loader_test.c
@@ -7,16 +7,20 @@ int main()
 
 	printf("\n");
 	printf("This is a C program loading C++ shared library libplumloader.\n");
-	printf("Vertex count: %d\n", mesh.vertex_count);
-	printf("Face count: %d\n", mesh.face_count);
+	printf("Vertex count: %zu\n", mesh.vertex_count);
+	printf("Face count: %zu\n", mesh.face_count);
 	printf("Vertex array:\n");
-	for (int i=0; i<mesh.vertex_count; i++) {
-		printf("%f ", mesh.vertex_array[i]);
+	for (size_t i=0; i<mesh.vertex_count; i++) {
+		float* v = mesh.vertex_array[i];
+		printf("%f %f %f\n", v[0], v[1], v[2]);
 	}
 	printf("\n");
 	printf("Face array:\n");
-	for (int i=0; i<mesh.face_count; i++) {
-		printf("%d ", mesh.face_array[i]);
+	for (size_t i=0; i<mesh.face_count; i++) {
+		int size = mesh.face_array[i][0];
+		for (int j=0; j<size; j++)
+			printf("%d ",mesh.face_array[i][j+1]);
+		printf("\n");
 	}
 	printf("\n");
 
