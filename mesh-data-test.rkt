560b13f0a3f0eb9f4fd19df07fe569943bda6d64
Stephen Kim <stephen422@gmail.com>
2015-09-04 13:24:24 +0900

    Break lines for better readability

--- mesh-data-test.rkt
+++ mesh-data-test.rkt
@@ -10,21 +10,20 @@
 
 (test-case
  "Basic mesh access"
-
- (check-equal? (get-vertex test-mesh 5) #(-0.5 0.5 -0.5))
-
- (check-equal? (get-face test-mesh 23) #(4 0 11))
-
- (check-equal? (get-face-count test-mesh) 24)
-
- (check-equal? (get-face-vertex test-face 1 test-vertices) #(-0.5 -0.5 0.5))
-
- (check-equal? (get-face-vertex-count test-face) 3))
+ (check-equal? (get-vertex test-mesh 5)
+	       #(-0.5 0.5 -0.5))
+ (check-equal? (get-face test-mesh 23)
+	       #(4 0 11))
+ (check-equal? (get-face-count test-mesh)
+	       24)
+ (check-equal? (get-face-vertex test-face 1 test-vertices)
+	       #(-0.5 -0.5 0.5))
+ (check-equal? (get-face-vertex-count test-face)
+	       3))
 
 (test-case
  "Mesh to VBO conversion"
-
  (check-equal? (vector-length (vboify test-mesh))
 	       (* (get-face-count test-mesh) 3 3))
-
- (check-equal? (vector-flatten #(#(1 2) #(3 4) #(5 6))) #(1 2 3 4 5 6)))
+ (check-equal? (vector-flatten #(#(1 2) #(3 4) #(5 6)))
+	       #(1 2 3 4 5 6)))
